27e24cc4f1f75aff768c7d4aa5e71eeed0edf5ec
Sided911 <51265600+Sided911@users.noreply.github.com>
2023-03-09 11:56:21 +0200

    3 zadanie gotovoe

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("n=");
+            byte n = byte.Parse(Console.ReadLine());
+            double[] array = new double[n];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = -50 + 100 * rnd.NextDouble();
+                Console.WriteLine($"array [{i}]) ={array[i]}");
+            }
+            Console.WriteLine($"array[0] = {array[0]}\tarray[{array.Length - 1}]={array[array.Length - 1]}");
         }
     }
 }
