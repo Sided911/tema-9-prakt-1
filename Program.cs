74f6013f38b49f3796d6b8bf3c7a5b7b739262a5
Sided911 <51265600+Sided911@users.noreply.github.com>
2023-03-09 10:48:13 +0200

    1 zadanie

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[6];
+            for (int i=0; i < 6; i++)
+            {
+                Console.Write($"array[{i}]=");
+                array[i] = int.Parse(Console.ReadLine()); 
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"array[{i}]) ={array[i]}");
+            }
+            Console.Read();
         }
     }
 }
