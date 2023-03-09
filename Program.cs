7f545a650226446be3f1c480c3daac4021a612f4
Sided911 <51265600+Sided911@users.noreply.github.com>
2023-03-09 11:11:26 +0200

    2 zadanie

--- Program.cs
+++ Program.cs
@@ -6,17 +6,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] array = new int[6];
-            for (int i=0; i < 6; i++)
-            {
-                Console.Write($"array[{i}]=");
-                array[i] = int.Parse(Console.ReadLine()); 
-            }
-            for (int i = 0; i < 6; i++)
-            {
-                Console.WriteLine($"array[{i}]) ={array[i]}");
-            }
-            Console.Read();
+            //1 задание
+            //int[] array = new int[6];
+            //for (int i=0; i < 6; i++)
+            //{
+            //    Console.Write($"array[{i}]=");
+            //    array[i] = int.Parse(Console.ReadLine()); 
+            //}
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]) ={array[i]}");
+            //}
+            //Console.Read();
+
+            //2 задание 
+            //int[] array = new int[12];
+            //Random rnd = new Random();
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    array[i] = rnd.Next(163, 190);
+            //}
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
         }
     }
 }
